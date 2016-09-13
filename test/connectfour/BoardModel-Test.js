50e172e3c481f8152a58c9887cf3ab1a137ceb64
Florian FERBACH <florian@marmelab.com>
2016-09-13 17:58:49 +0200

    Split BoardModel tests by method

--- test/connectfour/BoardModel-Test.js
+++ test/connectfour/BoardModel-Test.js
@@ -3,25 +3,32 @@ import { expect } from 'chai';
 import BoardModel from '../../src/connectfour/BoardModel';
 
 describe('Board', () => {
-    it('should initialize correctly', () => {
-        const board = new BoardModel(2, 2);
+    describe('constructor', () => {
+        it('should use width and height parameters', () => {
+            const board = new BoardModel(2, 2);
 
-        expect(board.cells.length).to.be.equal(2);
+            expect(board.cells.length).to.be.equal(2);
 
-        for (let x = 0; x < board.cells.length; x += 1) {
-            expect(board.cells[x].length).to.be.equal(2);
-        }
+            for (let x = 0; x < board.cells.length; x += 1) {
+                expect(board.cells[x].length).to.be.equal(2);
+            }
+        });
     });
 
-    it('should be able to have a disc added', () => {
-        const board = new BoardModel(2, 2);
-        board.addDisc(0, board.colors.red);
-        expect(board.cells[0][1]).not.to.be.equal(0);
+    describe('addDisc', () => {
+        it('should add a disc to the right column', () => {
+            const board = new BoardModel(2, 2);
+            board.addDisc(0, board.colors.red);
+            expect(board.cells[0][1]).not.to.be.equal(0);
+        });
     });
 
-    it('should say it when it\'s full', () => {
-        const board = new BoardModel(1, 1);
-        board.addDisc(0, board.colors.red);
-        expect(board.isFull()).to.be.true;
+    describe('isFull', () => {
+        it('should recognize when a board is full', () => {
+            const board = new BoardModel(1, 1);
+            expect(board.isFull()).to.be.false;
+            board.addDisc(0, board.colors.red);
+            expect(board.isFull()).to.be.true;
+        });
     });
 });
