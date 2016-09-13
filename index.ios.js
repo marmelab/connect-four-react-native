01be1a7868c02b149ffd34c4fbdd0aec52d67ba4
Florian FERBACH <florian@marmelab.com>
2016-09-13 10:29:05 +0200

    Fix eslint warnings

--- index.ios.js
+++ index.ios.js
@@ -1,47 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Navigator
+  Navigator,
 } from 'react-native';
 
 import WelcomePage from './src/WelcomePage';
 
 export default class ConnectFour extends Component {
-
-    render() {
-        return (
-            <Navigator
-                initialRoute={{id: 'WelcomePage', name: 'Index'}}
-                renderScene={this.renderScene}
-                configureScene={this.configureScene}
-            />
-        );
-    }
-
-    renderScene(route, navigator) {
-        var routeId = route.id;
-        if (routeId === 'WelcomePage') {
-            return (
-                <WelcomePage
-                    navigator={navigator} />
-            );
-        }
-    }
-
-    configureScene(route){
+    static configureScene(route) {
         if (route.sceneConfig) {
             return route.sceneConfig;
         }
         return Navigator.SceneConfigs.FloatFromRight;
+    }
+
+    static defaultPage(navigator) {
+        return (
+            <WelcomePage navigator={navigator} />
+        );
+    }
+
+    renderScene(route, navigator) {
+        const routeId = route.id;
+        if (routeId === 'WelcomePage') {
+            return this.defaultPage(navigator);
+        }
+        return this.defaultPage(navigator);
+    }
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ id: 'WelcomePage', name: 'Index' }}
+                renderScene={this.renderScene}
+                configureScene={this.configureScene}
+            />
+        );
     }
 
 }
---
+++ src/Button.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#5cb85c',
+        borderRadius: 20,
+        padding: 10,
+    },
+    text: {
+        fontSize: 22,
+        color: 'white',
+    },
+});
+
+const Button = ({ onPress, text }) => (
+    <View>
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+            <Text style={styles.text}>{text}</Text>
+        </TouchableOpacity>
+    </View>
+);
+
+Button.propTypes = {
+    onPress: React.PropTypes.func.isRequired,
+    text: React.PropTypes.string.isRequired,
+};
