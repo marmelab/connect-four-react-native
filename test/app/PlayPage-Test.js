911b5e4c93152367e44c9fafddce49cc8685fb66
Florian FERBACH <florian@marmelab.com>
2016-09-14 17:55:41 +0200

    Add test on player alternance

--- test/app/PlayPage-Test.js
+++ test/app/PlayPage-Test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import PlayPage from '../../src/app/PlayPage';
+import PlayerBadge from '../../src/connectfour/PlayerBadge';
+import Button from '../../src/chrome/Button';
 
 describe('<PlayPage />', () => {
     it('should render correctly', () => {
@@ -11,5 +13,17 @@ describe('<PlayPage />', () => {
         expect(playPage.length).to.equal(1);
         expect(playPage.contains(Text)).to.equal(true);
         expect(playPage.find(Text).props().children).to.equal('Play');
+    });
+
+    it('should alternate turns when I click on the action button', () => {
+        const playPage = shallow(<PlayPage />);
+
+        const currentPlayerName = playPage.find(PlayerBadge).filter('[highlighted=true]').props().player.name;
+
+        playPage.find(Button).first().simulate('press');
+
+        const otherPlayerName = playPage.find(PlayerBadge).filter('[highlighted=true]').props().player.name;
+
+        expect(currentPlayerName).not.to.be.equal(otherPlayerName);
     });
 });
