3451d63c63939a9edbb858011ed46acbbe2fa95a
Florian FERBACH <florian@marmelab.com>
2016-09-15 10:38:14 +0200

    Remove unnecessary test

--- test/app/PlayPage-Test.js
+++ test/app/PlayPage-Test.js
@@ -14,16 +14,4 @@ describe('<PlayPage />', () => {
         expect(playPage.contains(Text)).to.equal(true);
         expect(playPage.find(Text).props().children).to.equal('Play');
     });
-
-    it('should alternate turns when I click on the action button', () => {
-        const playPage = shallow(<PlayPage />);
-
-        const currentPlayerName = playPage.find(PlayerBadge).filter('[highlighted=true]').props().player.name;
-
-        playPage.find(Button).first().simulate('press');
-
-        const otherPlayerName = playPage.find(PlayerBadge).filter('[highlighted=true]').props().player.name;
-
-        expect(currentPlayerName).not.to.be.equal(otherPlayerName);
-    });
 });
