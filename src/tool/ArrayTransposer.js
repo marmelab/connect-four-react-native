2f715fda410989114cef562d62fc67f822b97952
Florian FERBACH <florian@marmelab.com>
2016-09-16 09:26:39 +0200

    Fix code style

--- src/tool/ArrayTransposer.js
+++ src/tool/ArrayTransposer.js
@@ -7,19 +7,20 @@ export const transposeHorizontally = array =>
 
 export const transposeDiagonally = (array, bottomToTop = false) => {
     const transposedCells = [];
-
-
     const yLength = array.length;
     const xLength = array[0].length;
     const maxLength = Math.max(xLength, yLength);
+
     for (let k = 0; k <= 2 * (maxLength); k += 1) {
         const row = [];
+
         for (let y = yLength - 1; y >= 0; y -= 1) {
             const x = k - (bottomToTop ? yLength - y : y);
             if (x >= 0 && x < xLength) {
                 row.push(array[y][x]);
             }
         }
+
         if (row.length > 0) {
             transposedCells.push(row);
         }
