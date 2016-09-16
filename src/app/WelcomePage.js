f9ad7c863bc8b0d9fe4e2d059eedbde964c046d6
Florian FERBACH <florian@marmelab.com>
2016-09-16 09:15:28 +0200

    Fix back navigation on play view

--- src/app/WelcomePage.js
+++ src/app/WelcomePage.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
 
 const WelcomePage = ({ navigator }) => {
     const startPlaying = () => {
-        navigator.push({
+        navigator.replace({
             id: 'PlayPage',
         });
     };
